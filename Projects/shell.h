f5620ca229005a7b3f48fe808357d5245f4a79f4
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-04-08 10:08:03 +0300

    Update shell.h

--- Projects/shell.h
+++ Projects/shell.h
@@ -1,5 +1,12 @@
 #ifndef SHELL_H
 #define SHELL_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
 /* prototype for scanning input*/
 #define EOF             (-1)
 #define ERRCHAR         ( 0)
