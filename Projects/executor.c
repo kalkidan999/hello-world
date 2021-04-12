73ca8733837da092821464a82586cc169f862671
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-04-12 15:02:52 +0300

    Update executor.c

--- Projects/executor.c
+++ Projects/executor.c
@@ -158,3 +158,36 @@ int do_simple_command(struct node_s *node)
     
     return 1;
 }
+/**
+ * change_dir - Change Dirctorie
+ * @cmd: Parsed Command
+ * @er: Statue Last Command Excuted
+ * Return: 0 Succes 1 Failed (For Old Pwd Always 0 Case No Old PWD)
+ */
+int change_dir(char **cmd, __attribute__((unused))int er)
+{
+	int value = -1;
+	char cwd[PATH_MAX];
+
+	if (cmd[1] == NULL)
+		value = chdir(getenv("HOME"));
+	else if (_strcmp(cmd[1], "-") == 0)
+	{
+		value = chdir(getenv("OLDPWD"));
+	}
+	else
+		value = chdir(cmd[1]);
+
+	if (value == -1)
+	{
+		perror("hsh");
+		return (-1);
+	}
+	else if (value != -1)
+	{
+		getcwd(cwd, sizeof(cwd));
+		setenv("OLDPWD", getenv("PWD"), 1);
+		setenv("PWD", cwd, 1);
+	}
+	return (0);
+}
