39623617b8f0c354c6f711babbb21d9199cd4771
kalkidan999 <45605340+kalkidan999@users.noreply.github.com>
2021-04-12 10:32:17 +0300

    Update scanner.c

--- Projects/scanner.c
+++ Projects/scanner.c
@@ -2,12 +2,13 @@
 char *tok_buf = NULL;
 int   tok_bufsize  = 0;
 int   tok_bufindex = -1;
-/* special token to indicate end of input 
-struct token_s eof_token =
+/* special token to indicate end of input  */
+struct token_s eof_token;
+eof_token = ( token_s)
 {
-    token_s.text_len = 0,
+   .text_len = 0,
 };
- */
+
 void add_to_buf(char c)
 {
     tok_buf[tok_bufindex++] = c;
